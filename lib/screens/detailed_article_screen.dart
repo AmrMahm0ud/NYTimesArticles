a8a15ca4382be820d22fcf3816236d6ed4e2a1a8
Amr <amrmahmoud710gmail.com>
2020-12-03 20:01:04 +0200

    added dummy String to detailed article screen

--- lib/screens/detailed_article_screen.dart
+++ lib/screens/detailed_article_screen.dart
@@ -16,7 +16,7 @@ class _DetailedArticleScreenState extends State<DetailedArticleScreen> {
         backgroundColor: Color(0xFF45E3C2),
         title: Text("NY Times Most Popular"),
       ),
-      body: Column(
+      body: ListView(
         children: [
           Padding(
             padding: const EdgeInsets.all(9.0),
@@ -100,7 +100,19 @@ class _DetailedArticleScreenState extends State<DetailedArticleScreen> {
               ),
             ),
           ),
+          dummyString(),
         ],
+      ),
+    );
+  }
+ ///////////////////////////////////////////
+ ////////////////Helper Widgets////////////
+ ////////////////////////////////////////
+
+  Widget dummyString(){
+    return Padding(
+      padding: const EdgeInsets.all(9.0),
+      child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
       ),
     );
   }
